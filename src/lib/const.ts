3f219712a54dcbae6d55ee98a45819a8111c7f9f
duskalor <duskalor1@gmail.com>
2025-01-27 02:36:15 -0500

    Add Tailwind CSS support, introduce header and hero components, and include new images for the site

---
+++ src/lib/const.ts
@@ -0,0 +1,63 @@
+import llama from '../assets/fotos/llama.jpg';
+import machupichu from '../assets/fotos/machupichu.jpg';
+import machu from '../assets/fotos/machu.jpg';
+
+export const fotos_machupichu = [
+  {
+    id: 1,
+    nombre: 'Machupichu',
+    descripcion:
+      'machupichu es un plato de comida de origen peruano, que se caracteriza por ser rico en proteínas y grasas, y por tener una textura suave y dulce. Machupichu es una comida popular en Perú y se utiliza en una variedad de platos, desde platos de comida rápida hasta platos de comida de alta calidad. Machupichu se puede comer en una variedad de formas, desde una taza de carne y frutas hasta una sopa de leche y huevos. Es una comida muy popular en Perú y se utiliza en una variedad de platos, desde platos de comida rápida hasta platos de comida de alta calidad.',
+
+    precio: 100,
+    imagen: machu,
+  },
+  {
+    id: 2,
+    nombre: 'Machupichu',
+    descripcion:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec malesuada lorem maximus mauris scelerisque, at rutrum nulla dictum. Ut ac ligula sapien. Suspendisse cursus faucibus finibus.',
+    precio: 100,
+    imagen: machupichu,
+  },
+  {
+    id: 3,
+    nombre: 'Machupichu y la llama',
+    descripcion:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec malesuada lorem maximus mauris scelerisque, at rutrum nulla dictum. Ut ac ligula sapien. Suspendisse cursus faucibus finibus.',
+    precio: 100,
+    imagen: llama,
+  },
+];
+
+export const links = [
+  {
+    id: 1,
+    nombre: 'Inicio',
+    href: '/',
+  },
+  {
+    id: 2,
+    nombre: 'Tours',
+    href: '/tours',
+  },
+  {
+    id: 3,
+    nombre: 'Nosotros',
+    href: '/nosotros',
+  },
+  // {
+  //   id: 4,
+  //   nombre: 'Contáctanos',
+  //   href: '/contactanos',
+  // },
+  {
+    id: 5,
+    nombre: 'Blog',
+    href: '/blog',
+  },
+];
+
+export const numero_contacto = '+51986154999';
+export const mensaje_contacto =
+  'Hola, quiero contactarte para que seas mi guia, en mi estadia en Cusco';
